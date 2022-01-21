cbf0fde9c2492dff49b6d2200f4a8b3512168038
Zeyan Li 李则言 <li_zeyan@icloud.com>
2022-01-21 11:45:37 +0800

    add a reset script for workload

---
+++ workload/reset.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2164
+cd "$(dirname "$0")"
+docker build . -t docker.peidan.me/lizytalk/train-ticket-bot:latest
+docker push docker.peidan.me/lizytalk/train-ticket-bot:latest
+kubectl delete -n train-ticket-bot -f bots.yml
+kubectl apply -n train-ticket-bot -f bots.ymll
